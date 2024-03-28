209751166e290e3947bc6af100316e739e003124
JKW1008 <todendvv91@gmail.com>
2024-03-28 17:13:59 +0900

    test

---
+++ 5.String/main.py
@@ -0,0 +1,39 @@
+# 문자열 slicing
+
+# a = "왕초보 홈페이지 만들기"
+# num = "0123456789"
+
+# zero based indexing
+# one based indexing
+
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[-1])
+# print(a[-2])
+# print(a[-3])
+
+# a[0:3]  # 0 이상 3 미만
+# print(a[1:4])  # 1 이상 4 미만
+# print(a[:4])  # ~ 4 미만
+# print(a[1:])  # 1 이상 ~
+# b = a[1:]
+# print(b)
+# b = a[:]
+# print(b)
+
+# "문자열" [시작 : 끝 : 간격]
+# print(a[::3])
+# print(num[::3])
+# print(num[::4])
+# print(num[::5])
+# print(num[::-2])
+# print(num[-2:-6:-1])
+
+a = "Hello World"
+
+
+# print(a.upper()) # 대문자
+# print(a.lower()) # 소문자
+
+# print(a.strip()) # 좌우 공백제거
