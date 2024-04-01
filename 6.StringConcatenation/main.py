09ca3d3ff57260badc7314bfc2e1b4522ffd28c5
JKW1008 <todendvv91@gmail.com>
2024-04-01 13:11:43 +0900

    6번강의

---
+++ 6.StringConcatenation/main.py
@@ -0,0 +1,22 @@
+# String Concatenation
+
+# a = "Hello"
+# b = "왕초보"
+
+# c = a + " " + b
+# print(c)
+
+# age = "23"
+# name = "마이클"
+
+# txt = "저는 {1} 세 입니다. 이름은 {0} 입니다."
+
+# print(txt.format(age, name))
+
+# Escape Characters
+
+a = "안녕하세요. 저는 \\\"19세\\\" 입니다."
+b = "\t안녕하세요. \n저는 마이클입니다."
+
+print(a)
+print(b)
