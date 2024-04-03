f218978aff693d2b925b11c2c4597d852e70ffc1
JKW1008 <todendvv91@gmail.com>
2024-04-03 10:11:07 +0900

    test

---
+++ 7.String_function/main.py
@@ -0,0 +1,29 @@
+# center(), count(), endswith(), expandtabs()
+
+# center()
+# a = "world!"
+
+# x = a.center(20)
+
+# print("01234567890123456789")
+# print(x)
+
+
+# count()
+# a = "저는 파이썬을 사랑합니다. 파이썬은 좋은언어죠."
+# x = a.count(" ")
+
+# print(x)
+
+
+# endswith()
+# a = "저는 파이썬을 사랑합니다. 파이썬은 좋은언어죠."
+# x = a.endswith("저는")
+
+# print(x)
+
+
+# expandtabs()
+# a = "h\te\tl\tl\to"
+
+# print(a.expandtabs(100))
