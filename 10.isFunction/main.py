268f515e5198a1dc770d6480d336af888eecd963
JKW1008 <todendvv91@gmail.com>
2024-04-08 23:58:54 +0900

    10.isFunction

---
+++ 10.isFunction/main.py
@@ -0,0 +1,13 @@
+# isalpha(), isnumeric(), isspace(), isalnum()
+
+a = "ABC"
+print(a.isalpha())
+
+b = "123"
+print(b.isnumeric())
+
+c = " "
+print(c.isspace())
+
+d = "1234abcd"
+print(d.isalnum())
