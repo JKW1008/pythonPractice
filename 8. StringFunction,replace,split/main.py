71919322d14f5bd3571c1a507c53b5c722323f82
JKW1008 <todendvv91@gmail.com>
2024-04-08 23:33:12 +0900

    8. StringFunction,replace,split

---
+++ 8. StringFunction,replace,split/main.py
@@ -0,0 +1,23 @@
+# replace(), split()
+
+
+# replace()
+# a = "안녕하세요. 킹초보 채널에 오신것을 환영합니다."
+
+# print(a.replace("킹초보", "왕초보"))
+
+# b = a.replace("킹초보", "왕초보")
+
+# a = "aaaa bbbb cccc"
+# b = a.replace("b", "A")
+
+# print(b)
+
+# split()
+
+a = "Hello, World!"
+b = a.split(",")
+
+print(type(b))
+
+# 파이썬에서 배열 (Array) 는 list 라고 한다
