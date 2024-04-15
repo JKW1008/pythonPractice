a2735f349378e592a5daaeb9080e3433b37c8507
JKW1008 <todendvv91@gmail.com>
2024-04-16 00:36:05 +0900

    chap 14

---
+++ 14.PythonOperator/main.py
@@ -0,0 +1,42 @@
+# 파이썬 연산자
+
+# is
+# x = ["apple", "orange"]
+# y = ["apple", "orange"]
+# z = x
+
+# print(x is y)
+# print(x is not y)
+
+# 멤버십 연산자 (in)
+# x = ["apple", "orange"]
+# print("banana" in x)
+# print("orange" in x)
+# print("banana" not in x)
+# print("orange" not in x)
+
+# Bitwise
+# & | ^ ~ << >>
+# print(6 & 3)
+# print(6 | 3)
+# print(6 ^ 3)
+# print(~3)
+# print(3 << 2)
+# print(12 >> 2)
+
+# 연산자 우선순위
+"""
+()
+""
++x -x ~x
+* / // %
++ -
+<< >>
+&
+^
+|
+== != >= < <= not in
+not
+and
+or
+"""
