9bb5db99d638a6a22426b4eb14ba0b3c54ab69a8
JKW1008 <todendvv91@gmail.com>
2024-04-28 02:34:06 +0900

    Set #2 method

--- 17.Set/main.py
+++ 17.Set/main.py
@@ -13,7 +13,7 @@
 # print(a)
 # print(type(a))
 
-a = {"apple", "banana", "cherry"}
+# a = {"apple", "banana", "cherry"}
 
 # for x in a:
 #     print(x)
@@ -30,8 +30,46 @@ a = {"apple", "banana", "cherry"}
 # a.update(b)
 # print(a)
 
-a.remove("banana")
-a.remove("banana")
-a.discard("banana")
+# a.remove("banana")
+# a.remove("banana")
+# a.discard("banana")
 # b = a.pop()
+# print(a)
+
+
+# a = {"apple", "banana"}
+# b = {"mandarin", "pear"}
+# a.update(b)   스스로 변화
+# c = a.union(b)  새로운 배열을 할당
+# print(c)
+
+# a.clear()
+# b = a.copy()
+
+# print(b)
+
+
+# a = {"apple", "banana", "mango"}
+# b = {"banana", "apple"}
+# z = a.difference(b)
+# a.difference_update(b)
+# 차집합
+
+
+# c = a.intersection(b)
+# a.intersection_update(b)
+# 교집합
+
+# c = a.isdisjoint(b)
+
+# c = a.issubset(b)
+# c = a.issuperset(b)
+
+a = {1, 2, 3}
+b = {3, 4, 5}
+
+# c = a.symmetric_difference(b)
+a.symmetric_difference_update(b)
+# 합집합 - 교집합
+
 print(a)
