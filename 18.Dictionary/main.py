a8cd9b270789f5a5db2cfe5f4ac228cb46eda65d
JKW1008 <todendvv91@gmail.com>
2024-04-28 02:58:04 +0900

    Dictionary #2

--- 18.Dictionary/main.py
+++ 18.Dictionary/main.py
@@ -2,13 +2,13 @@
 # 3.6 unordered
 # 3.7 ordered
 
-a = {
-    "name": "smith",
-    "age": 19,
-    # "address": "Banpo Dong Seocho Gu Seoul",
-    "address": "Dachi Dong Kangnam Gu Seoul",
-    "hobby": ["Flower Art", "Movie", "Climing"]
-}
+# a = {
+#     "name": "smith",
+#     "age": 19,
+#     # "address": "Banpo Dong Seocho Gu Seoul",
+#     "address": "Dachi Dong Kangnam Gu Seoul",
+#     "hobby": ["Flower Art", "Movie", "Climing"]
+# }
 
 # print(a)
 # print(type(a))
@@ -57,5 +57,59 @@ a = {
 # for x in a:
 #     print(a[x])
 
-for x in a:
-    print(a.get(x))
+# for x in a:
+#     print(a.get(x))
+
+
+# -------------------------------------------------------------------------------------------------------------------------------
+
+# a = {
+#     "name": "smith",
+#     "age": "19",
+#     "address": "Dachi Dong Kangnam Gu Seoul",
+#     "hobby": "Flower Art, Movie, Climing"
+# }
+
+# for x in a.keys():
+#     print(x)
+
+# for x in a.values():
+#     print(x)
+
+# for x in a:
+#     print(a[x])
+
+# for x in a:
+#     print(x + ": " + a[x])
+
+# for key, value in a.items():
+#     print(key, ":", value)
+
+# b = a.copy()
+# b = dict(a)
+
+# print(b)
+
+# family = {
+#     "mom": {
+#         "name": "My mother",
+#         "age": 40
+#     },
+#     "dad": {
+#         "name": "My father",
+#         "age": 41
+#     }
+# }
+
+# print(family["dad"]["name"])
+
+
+a = {
+    "name": "smith",
+    "age": "19",
+    "year": 2022
+}
+
+x = a.setdefault("year", 2000)
+print(a)
+print(x)
