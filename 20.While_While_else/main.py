bfc59d5f96e2535aba711eb7b99da0f0c6b1a0ff
JKW1008 <todendvv91@gmail.com>
2024-04-28 03:20:44 +0900

    Python While and While else

---
+++ 20.While_While_else/main.py
@@ -0,0 +1,16 @@
+# While Loop
+
+i = 1
+# while i < 10:
+#     i += 1
+#     if i == 5:
+#         # break
+#         continue
+#     # i += 1
+#     print(i)
+
+while i < 5:
+    print(i)
+    i = i + 1
+else:
+    print("loop end")
